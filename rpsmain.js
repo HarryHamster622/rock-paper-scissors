9a87b94fd89e1faff69350683b700bd29c3aaba2
HarryHamster622 <56613758+HarryHamster622@users.noreply.github.com>
2023-08-31 08:35:31 -0400

    Finish code for rounds and printing who wins

--- rpsmain.js
+++ rpsmain.js
@@ -4,12 +4,33 @@ function getComputerChoice() {
     return computerPick;
 }
 
+let wins = 0;
+let losses = 0;
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper') {
-        console.log("You lose! Paper beats rock!")
+        console.log("You lose! Paper beats rock!");
+        losses++;
     } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') {
-        console.log("You win! Rock beats scissors!")
+        console.log("You win! Rock beats scissors!");
+        wins++;
     } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'rock') {
-        console.log("It's a tie! We both picked rock!")
+        console.log("It's a tie! We both picked rock!");
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') {
+        console.log("You win! Paper beats rock!");
+        wins++;
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors') {
+        console.log("You lose! Scissors beats paper!");
+        losses++;
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'paper') {
+        console.log("It's a tie! We both picked paper!");
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock') {
+        console.log("You lose! Rock beats scissors!");
+        losses++;
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper') {
+        console.log("You win! Scissors beats paper!");
+        wins++;
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'scissors') {
+        console.log("It's a tie! We both picked scissors!")
     }
 }
