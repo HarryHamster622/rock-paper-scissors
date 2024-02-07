e295956e2d3063d4ffd1c25a30a7bd5fe282c4bc
HarryHamster622 <56613758+HarryHamster622@users.noreply.github.com>
2024-02-07 11:30:00 -0500

    Complete JS DOM logic and rewrite readme.md

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # rock-paper-scissors
 
-This is a simple console game written entirely in javascript. Yes I know I could've used a switch instead of repeated else if statements, however switch cases were not a part of the program up to this point. 
-
-By typing "game()" (without the quotations) in the console you will play a series of 5 rounds of "rock, paper, scissors!" against the computer, which will tally your wins and losses
+This is a simple rock paper scissors game written in Javascript. I did not style at all because the project was more of a focus on javascript. The screen will display the results of each round and then a winner (first player to 5 points)
--- rpsmain.js
+++ rpsmain.js
@@ -7,31 +7,43 @@ function getComputerChoice() {
 let wins = 0;
 let losses = 0;
 let resultsDiv = document.querySelector('#results');
+let playerScore = document.querySelector('#playerScore');
+let computerScore = document.querySelector('#computerScore');
+let finalWinner = document.querySelector('#finalScore');
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper') {
         resultsDiv.textContent = "You lose this round! Paper beats rock!";
         losses++;
     } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') {
-        resultsDiv.textContent = "You win! Rock beats scissors!";
+        resultsDiv.textContent = "You win this round! Rock beats scissors!";
         wins++;
     } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'rock') {
         resultsDiv.textContent = "It's a tie! We both picked rock!";
     } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') {
-        resultsDiv.textContent = "You win! Paper beats rock!";
+        resultsDiv.textContent = "You win this round! Paper beats rock!";
         wins++;
     } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors') {
-        resultsDiv.textContent = "You lose! Scissors beats paper!";
+        resultsDiv.textContent = "You lose this round! Scissors beats paper!";
         losses++;
     } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'paper') {
         resultsDiv.textContent = "It's a tie! We both picked paper!";
     } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock') {
-        resultsDiv.textContent = "You lose! Rock beats scissors!";
+        resultsDiv.textContent = "You lose this round! Rock beats scissors!";
         losses++;
     } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper') {
-        resultsDiv.textContent = "You win! Scissors beats paper!";
+        resultsDiv.textContent = "You win this round! Scissors beats paper!";
         wins++;
     } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'scissors') {
         resultsDiv.textContent = "It's a tie! We both picked scissors!";
     }
+    playerScore.textContent = "Player: " + wins;
+    computerScore.textContent = "Computer: " + losses;
+
+    if (wins == 5) {
+        finalWinner.textContent = "You win! " + wins + ' - ' + losses;
+    } else if (losses == 5) {
+        finalWinner.textContent = 'You lose! ' + losses + ' - ' + wins;
+    }
+
 }
